8051f38c299e12191fbd84e6981edd26f1b937cc
Jaroslav Petera <jaroslav.petera@profiq.com>
2025-04-10 08:40:20 +0000

    feat: add slash command functionality

--- functions/src/slackApp.ts
+++ functions/src/slackApp.ts
@@ -1,4 +1,5 @@
 import { App, ExpressReceiver } from '@slack/bolt';
+import { AnyBlock } from '@slack/types';
 import config from './config';
 
 const receiver = new ExpressReceiver({
@@ -24,6 +25,67 @@ app.message('hello', async ({ message, say }) => {
   if (message.subtype === undefined || message.subtype === 'bot_message') {
     await say(`Hello <@${message.user}>!`);
   }
+});
+
+// Format: /poll "Question" "," list of options separated by ","
+app.command('/poll', async ({ command, ack, respond }) => {
+  await ack();
+
+  const text = command.text || '';
+
+  const parts = text.split(',').map((str) => str.trim());
+
+  // TODO: add check if it is the right format
+
+  const question = parts[0];
+  const options = parts.slice(1);
+
+  const blocks: AnyBlock[] = [];
+
+  blocks.push({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `*${question}*`,
+    },
+  });
+
+  blocks.push({ type: 'divider' });
+
+  for (let i = 0; i < options.length; i++) {
+    const option = options[i];
+
+    blocks.push({
+      type: 'section',
+      text: {
+        type: 'plain_text',
+        text: option,
+      },
+      accessory: {
+        type: 'button',
+        text: {
+          type: 'plain_text',
+          text: 'Vote',
+        },
+        action_id: i.toString(),
+      },
+    });
+  }
+
+  blocks.push({ type: 'divider' });
+
+  blocks.push({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: `Poll created by *<@${command.user_id}>*`,
+    },
+  });
+
+  await respond({
+    response_type: 'in_channel',
+    blocks,
+  });
 });
 
 export const slackReceiver = receiver;
