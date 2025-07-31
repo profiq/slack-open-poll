ff441ef8ef74322ab80441b06bd5d2350ec35cb8
robin <robin.tokarsky@profiq.com>
2025-07-31 15:48:38 +0200

    feat: add route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,27 @@
-import { Button } from "@/components/ui/button"
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center">
-            <Button>Click me</Button>
-        </div>
-    )
+        <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/poll/:pollId" element={<PollDetailPlaceholder />} />
+                    <Route path="/login" element={<Login />} />
+                </Routes>
+        </BrowserRouter>
+    );
+}
+
+function Dashboard() {
+    return <h1>Dashboard</h1>;
+}
+
+function PollDetailPlaceholder() {
+    return <h1>Poll Detail Placeholder</h1>;
+}
+
+function Login() {
+    return <h1>Login</h1>;
 }
 
 export default App
