9cde6498692a534014b0bf77eaea20b0610fc724
robin <robin.tokarsky@profiq.com>
2025-08-18 10:13:10 +0200

    fix: fix issue

--- web/src/components/authContext.tsx
+++ web/src/components/authContext.tsx
@@ -46,7 +46,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
     return (
         <AuthContext.Provider value={value}>
-            {!loading && children}
+            {children}
         </AuthContext.Provider>
     );
 }
