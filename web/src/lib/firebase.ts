a24bc6c5558b5c0a04187bbcd3b15de0f1367443
robin <robin.tokarsky@profiq.com>
2025-08-18 17:23:03 +0200

    fix: fix issue

--- web/src/lib/firebase.ts
+++ web/src/lib/firebase.ts
@@ -15,12 +15,13 @@ const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-const useEmulator = import.meta.env.VITE_USE_EMULATORS === "true";
-if (import.meta.env.DEV && useEmulator) {
+const useEmulator = Boolean(import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST);
+
+if (useEmulator) {
     const host = import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_HOST ?? "localhost";
     const port = import.meta.env.VITE_FIREBASE_AUTH_EMULATOR_PORT ?? "9099";
     connectAuthEmulator(auth, `http://${host}:${port}`);
-    console.log(`Firebase Auth Emulator running at http://${host}:${port}`);
 }
+
 
 export { auth };
